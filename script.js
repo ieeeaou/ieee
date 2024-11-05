e283f5b94fe9559d6930a9a9cc9014f0243b1d79
ieeeaou <ieeeaou@gmail.com>
2024-11-05 15:43:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-const menuToggle = document.getElementById('mobile-menu');
-const navList = document.querySelector('.nav-list');
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("mobile-menu");
+    const navList = document.querySelector(".nav-list");
+  
+    menuToggle.addEventListener("click", function () {
+      menuToggle.classList.toggle("active");
+      navList.classList.toggle("show");
+    });
+  });
 
-menuToggle.addEventListener('click', () => {
-    navList.classList.toggle('show');
-});
+  document.addEventListener('DOMContentLoaded', () => {
+    const sr = ScrollReveal({
+      reset: true,
+      duration: 2000,
+      distance: '50px',
+      interval: 100,
+    });
+  
+    sr.reveal('.home-section', {
+      origin: 'top',
+    });
+  
+    sr.reveal('.about-section', {
+      origin: 'left',
+    });
+  
+    sr.reveal('.ieee-aou-section', {
+      origin: 'bottom',
+    });
+  
+    sr.reveal('.ieee-section-container', {
+      origin: 'top',
+    });
+  });
+  
+  
