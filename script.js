24c49972e1e0eac5ccb679bc7a37af501ce6b956
ieeeaou <ieeeaou@gmail.com>
2024-11-07 14:11:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   
     sr.reveal('.ieee-section-container', {
-      origin: 'top',
+      origin: 'left',
     });
   });
   
