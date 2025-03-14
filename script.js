54f587bce527ef441fd02f041b7959285ff44628
ieeeaou <ieeeaou@gmail.com>
2025-03-14 21:21:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const sr = ScrollReveal({
-      reset: true,
-      duration: 2000,
-      distance: '50px',
-      interval: 100,
-    });
-  
-    sr.reveal('.home-content', { origin: 'left' });
-    sr.reveal('.about-content', { origin: 'left' });
-    sr.reveal('.ieee-aou-section', { origin: 'bottom' });
-    sr.reveal('.ieee-section-container', { origin: 'left' });
-
-    sr.reveal('.team-member', {origin: 'left',});
-
-    sr.reveal('.join-video', { origin: 'left' });
-    sr.reveal('.membership-card', { origin: 'bottom', });
-  });
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const faqItems = document.querySelectorAll(".faq-item");
 
